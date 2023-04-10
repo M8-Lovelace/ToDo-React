fd17b47c7eb99faf6beacb8007804673b64b958a
Laura Susano <lauritasusano.10@gmail.com>
2023-04-10 16:24:12 -0500

    feat(project): modify project with better structure

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,67 +1,75 @@
 // Importaciones
-import { useState } from "react"
+import { useState } from "react";
 
 // Componentes
-import { CreateTodoButton } from "./components/CreateTodoButton/CreateTodoButton"
-import { TodoCounter } from "./components/TodoCounter/TodoCounter"
-import { TodoItem } from "./components/TodoItem/TodoItem"
-import { TodoList } from "./components/TodoList/TodoList"
-import { TodoSearch } from "./components/TodoSearch/TodoSearch"
+import { CreateTodoButton } from "../CreateTodoButton";
+import { TodoCounter } from "../TodoCounter";
+import { TodoItem } from "../TodoItem";
+import { TodoList } from "../TodoList";
+import { TodoSearch } from "../TodoSearch";
 
 const defaultTodos = [
-  { text: 'Aprender React', completed: false },
-  { text: 'Aprender React 2', completed: false },
-  { text: 'Aprender React 3', completed: true },
-]
+  { text: "Aprender React", completed: false },
+  { text: "Aprender React 2", completed: false },
+  { text: "Aprender React 3", completed: true },
+];
 
 const App = () => {
   // El "hook" método useState() devuelve un array con dos elementos
-  const [search, setSearch] = useState('');
+  const [search, setSearch] = useState("");
   const [todos, setTodos] = useState(defaultTodos);
 
   // Constantes que se enviarán al componente TodoCounter
-  const completedTodos = todos.filter(todo => todo.completed === true).length
-  const allTodos = todos.length
+  const completedTodos = todos.filter((todo) => todo.completed === true).length;
+  const allTodos = todos.length;
 
   // Validar si el input no tiene texto y mostrar todos los todos, de lo contrario, filtrarlos
   let searchedTodos = [];
   if (!(search.length >= 1)) {
     searchedTodos = todos;
   } else {
-    searchedTodos = todos.filter(todo => {
+    searchedTodos = todos.filter((todo) => {
       const todoText = todo.text.toLowerCase();
       if (todoText.includes(search.toLowerCase())) {
         return todo;
       }
-    })
+    });
   }
 
   // Cambiar el estados de los ToDos
   const toggleCompleteTodo = (text) => {
-    const todoIndex = todos.findIndex(todo => todo.text === text);
+    const todoIndex = todos.findIndex((todo) => todo.text === text);
     const newTodos = [...todos];
     newTodos[todoIndex].completed = !newTodos[todoIndex].completed;
     setTodos(newTodos);
-  }
+  };
 
   // Eliminar un ToDo
   const deleteTodo = (text) => {
-    const todoIndex = todos.findIndex(todo => todo.text === text);
-    const newTodos = [...todos]
+    const todoIndex = todos.findIndex((todo) => todo.text === text);
+    const newTodos = [...todos];
     newTodos.splice(todoIndex, 1);
     setTodos(newTodos);
-  }
+  };
 
   return (
     <>
       <TodoCounter completedTodos={completedTodos} allTodos={allTodos} />
       <TodoSearch search={search} setSearch={setSearch} />
       <TodoList>
-        {searchedTodos.map(todo => (<TodoItem onToggle={() => toggleCompleteTodo(todo.text)} onDelete={() => deleteTodo(todo.text)} key={todo.text} text={todo.text} completed={todo.completed} />))}
+        {searchedTodos.map((todo) => (
+          <TodoItem
+            onToggle={() => toggleCompleteTodo(todo.text)}
+            onDelete={() => deleteTodo(todo.text)}
+            key={todo.text}
+            text={todo.text}
+            completed={todo.completed}
+          />
+        ))}
       </TodoList>
       <CreateTodoButton />
     </>
-  )
-}
+  );
+};
 
 export { App };
---
+++ src/components/TodoCounter/index.jsx
@@ -0,0 +1,14 @@
+// Importamos el css
+import "./TodoCounter.css";
+
+const TodoCounter = ({ completedTodos, allTodos }) => {
+  return (
+    <>
+      <h2 className="TodoCounter">
+        Has completado {completedTodos} de {allTodos} TODOs
+      </h2>
+    </>
+  );
+};
+
+export { TodoCounter };
