694479ebcc6fabdec9946518eabe80ef1ef8d87e
Laura Susano <lauritasusano.10@gmail.com>
2023-04-14 12:27:52 -0500

    feat: setup deploy using gh-pages

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,8 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // GitHub Pages
+  base: "/ToDo-React/",
   resolve: {
     // Añadir alias en el proyecto
     alias: {
