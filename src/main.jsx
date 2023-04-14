82860e54d9595a4abda8c4a2eac3d43c224a3672
Laura Susano <lauritasusano.10@gmail.com>
2023-04-14 10:40:54 -0500

    refactor(app): change of file location

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { App } from "./components/App";
+import { App } from "./views/App";
 import "./main.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
